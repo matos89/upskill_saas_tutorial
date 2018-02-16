a8a1fc5e8283d2da194d3608e1d39c8a2d314dc7
Justin Matos <matos89@gmail.com>
2018-02-15 17:54:09 -0800

    fixed contact table name, added created action to controller to save to db

--- db/migrate/20180216014852_create_contacts.rb
+++ db/migrate/20180216014852_create_contacts.rb
@@ -1,6 +1,6 @@
-class CreateContracts < ActiveRecord::Migration[5.0]
+class CreateContacts < ActiveRecord::Migration[5.0]
   def change
-    create_table :contracts do |t|
+    create_table :contacts do |t|
       t.string :name
       t.string :email
       t.text :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216012312) do
+ActiveRecord::Schema.define(version: 20180216014852) do
 
-  create_table "contracts", force: :cascade do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
